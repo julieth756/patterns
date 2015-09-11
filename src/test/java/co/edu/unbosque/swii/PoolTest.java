e3b795e7fea6139f733f517cde35dacaddc9d8fd
clave_f212 <clave_f212@clavef212-OptiPlex-9020-AIO>
2015-09-10 21:13:59 -0500

    clase 13/08

--- src/test/java/co/edu/unbosque/swii/PoolTest.java
+++ src/test/java/co/edu/unbosque/swii/PoolTest.java
@@ -6,11 +6,14 @@
 package co.edu.unbosque.swii;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 import org.apache.commons.pool2.BaseObjectPool;
 import org.apache.commons.pool2.KeyedObjectPool;
 import org.apache.commons.pool2.ObjectPool;
+import org.apache.commons.pool2.PooledObject;
 import org.apache.commons.pool2.impl.GenericKeyedObjectPool;
 import org.apache.commons.pool2.impl.GenericObjectPool;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 /**
@@ -42,23 +45,32 @@ public class PoolTest {
         }                
     }
     
-    @Test
-    public void quePasaCuandoSeCierraUnaConexionAntesDeRetornarla(){
-        
+     @Test
+    public void quePasaCuandoSeCierraUnaConexionAntesDeRetornarla() throws Exception{
+        FabricaConexiones fc =new FabricaConexiones("aretico.com", 5432, "software2", "grupo3_5", pwd);
+        ObjectPool<Connection> pool=new GenericObjectPool<Connection>(fc);
+        Connection c=pool.borrowObject();
+        pool.close();
+        pool.returnObject(c); 
     }
     
     @Test
     public void quePasaCuandoSeRetornaUnaconexionContransaccionIniciada(){
-        
+        FabricaConexiones fc =new FabricaConexiones("aretico.com", 5432, "software2", "grupo3_5", pwd);
+      
     }
     
     @Test(threadPoolSize = 5, invocationCount = 5)
     public void midaTiemposParaInsertar1000RegistrosConSingleton(){
+        FabricaConexiones fc =new FabricaConexiones("aretico.com", 5432, "software2", "grupo3_5", pwd);
         
     }
     
     @Test(threadPoolSize = 5, invocationCount = 5)
-    public void midaTiemposParaInsertar1000RegistrosConObjectPool(){
-        
+    public void midaTiemposParaInsertar1000RegistrosConObjectPool() throws Exception{
+        FabricaConexiones fc =new FabricaConexiones("aretico.com", 5432, "software2", "grupo3_5", pwd);
+        ObjectPool<Connection> pool=new GenericObjectPool<Connection>(fc);
+        Connection c=pool.borrowObject();
+        pool.returnObject(c); 
     }
 }
